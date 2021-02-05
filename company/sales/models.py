82987bf0fe01b4128bb8e9bd36e847a224c0cd00
joelschutz <joeymarley@gmail.com>
2021-02-05 16:50:03 -0300

    Refactoring Models

--- company/sales/models.py
+++ company/sales/models.py
@@ -5,14 +5,38 @@ from django.contrib.auth.models import User
 class SalesPerson(User):
     name = models.CharField(max_length=200, null=False, blank=False)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Sales Person'
+
 class Client(User):
     name = models.CharField(max_length=200, null=False, blank=False)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Client'
+
 class Product(models.Model):
     name = models.CharField(verbose_name='Product name', max_length=300, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class License(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='licenses')
     client = models.ForeignKey('Client', on_delete=models.CASCADE, related_name='licenses')
     seller = models.ForeignKey('SalesPerson', on_delete=models.SET_NULL, related_name='licenses', null=True, blank=True)
-    expire_date = models.DateField(null=False, blank=False)
+    activation_date = models.DateField(null=False, blank=False)
+    valid_time = models.DurationField(null=False, blank=False)
+
+    @property
+    def expire_date(self):
+        return self.activation_date + self.valid_time
+
+    def __str__(self):
+        return f'{self.product} - {self.client}'
