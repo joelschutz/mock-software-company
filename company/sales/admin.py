54129c2df981d1c623a434cdca4060461fc217f4
joelschutz <joeymarley@gmail.com>
2021-02-05 16:49:55 -0300

    Adding models to the admin

--- company/sales/admin.py
+++ company/sales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from sales.models import SalesPerson, Client, Product, License
 
 # Register your models here.
+admin.site.register(SalesPerson)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(License)
