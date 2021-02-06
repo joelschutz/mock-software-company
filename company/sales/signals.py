f8f9ce1c475ec29ad9343d43381757f0523ca50f
joelschutz <joeymarley@gmail.com>
2021-02-06 11:47:54 -0300

    New object signal fix

--- company/sales/signals.py
+++ company/sales/signals.py
@@ -2,12 +2,16 @@ from datetime import date
 
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 from sales.models import License
 
 @receiver(pre_save, sender=License)
 def set_activation_date(sender, instance, **kwargs):
-    active_before = sender.objects.get(id=instance.id).activated
+    try:
+        active_before = sender.objects.get(id=instance.id).activated
+    except ObjectDoesNotExist:
+        return
     active_after = instance.activated
     if (not active_before) and active_after:
         instance.activation_date = date.today()
