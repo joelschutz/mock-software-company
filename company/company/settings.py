d364a5df5ec2d937b686e05d2496a2c72bd97794
joelschutz <joeymarley@gmail.com>
2021-02-06 16:21:03 -0300

    Email sending implementation

--- company/company/settings.py
+++ company/company/settings.py
@@ -129,3 +129,11 @@ Q_CLUSTER = {
     "name": "Mock Company",
     "orm": "default",
 }
+
+# E-mail Configuration
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
--- company/email_notifier/tasks.py
+++ company/email_notifier/tasks.py
@@ -1,4 +1,7 @@
 import datetime
+from templated_email import send_templated_mail, get_templated_mail
+
+from company.settings import EMAIL_HOST_USER
 
 from sales.models import License
 
@@ -8,7 +11,7 @@ time_deltas = {
     '1 Week': datetime.timedelta(weeks=1)
 }
 
-def check_licenses(target_date=datetime.date.today()):
+def check_licenses(target_date):
     active_licenses = License.objects.filter(activated=True).all()
     scheduled_sellers = {}
 
@@ -17,17 +20,33 @@ def check_licenses(target_date=datetime.date.today()):
         reason = None
 
         if remaining_active_time == time_deltas['4 Months']:
-            reason = 'Exactly 4 months to expire.'
-        elif (remaining_active_time <= time_deltas['1 Month']) and (target_date.weekday() == 0):
-            reason = 'Monday task, 1 month to expire.'
-        elif remaining_active_time <= time_deltas['1 Week']:
-            reason = 'Expiring soon'
+            reason = 'Expira em 4 meses'
+        if (remaining_active_time <= time_deltas['1 Month']) and (target_date.weekday() == 0):
+            reason = 'É segunda feira e falta um mês ou menos expirar'
+        if remaining_active_time <= time_deltas['1 Week']:
+            reason = 'Expira em breve'
         
         if reason:
             try:
-                scheduled_sellers[active_license.seller][active_license.client].append((active_license, reason))
+                scheduled_sellers[active_license.seller].append((active_license, reason))
             except KeyError:
-                scheduled_sellers[active_license.seller] = {active_license.client:[(active_license, reason)]}
+                scheduled_sellers[active_license.seller] = [(active_license, reason)]
     
     return scheduled_sellers
         
+def send_email(seller, licenses):
+    message = get_templated_mail(
+        template_name='notify_seller',
+        from_email=EMAIL_HOST_USER,
+        to=[seller.email],
+        context={
+            'seller':seller,
+            'licenses':licenses
+        },)
+    message.send()
+
+def notify_sellers(target_date=datetime.date.today()):
+    scheduled_sellers = check_licenses(target_date)
+
+    for seller in scheduled_sellers.keys():
+        send_email(seller, scheduled_sellers[seller])
